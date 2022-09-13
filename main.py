f246413d9225e628b137273f26e0705ba1a07837
Dr-Insanity <67900447+Dr-Insanity@users.noreply.github.com>
2022-09-13 13:23:18 +0200

    s

--- main.py
+++ main.py
@@ -90,16 +90,22 @@ if token is None:
 
 bot = commands.InteractionBot()
 
+class ButtonMK_Q(disnake.ui.Button):
+    def __init__(self, bot: commands.InteractionBot, amount: int):
+        self.bot = bot
+        self.amount = amount
+
+    @disnake.ui.button(label='Start writing the questions', custom_id=f"answer_questions_page1", style=disnake.ButtonStyle.blurple)
+    async def letsgoow(self, button: disnake.ui.Button, i: disnake.MessageInteraction):
+        await i.response.send_message(embed=disnake.Embed(title=f"Leeet's Gooooooooooowww", description=f"Let's go let's go let's go!!!!"))
+
 class mk_q(disnake.ui.View):
     def __init__(self, bot: commands.InteractionBot, amount: int):
         self.bot = bot
         self.amount = amount
         self.children = []
         self.timeout = 0
-        self.add_item(self.letsgoow)
-    @disnake.ui.button(label='Start writing the questions', custom_id=f"answer_questions_page1", style=disnake.ButtonStyle.blurple)
-    async def letsgoow(self, button: disnake.ui.Button, i: disnake.MessageInteraction):
-        await i.response.send_message(embed=disnake.Embed(title=f"Leeet's Gooooooooooowww", description=f"Let's go let's go let's go!!!!"))
+        self.add_item(ButtonMK_Q(self.bot, self.amount))
 
 class startmk_q(disnake.ui.View):
     def __init__(self, bot: commands.InteractionBot, howmanyleft: int):
